a83dcb9198b617bbe14ad580edd6e2bd0d83f0bd
raz <raz@zgen.hu>
2024-10-19 12:23:41 +0200

    Hotfix 3

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import { compression } from 'vite-plugin-compression2'
 import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
+import path from 'path'
 
 export default defineConfig({
   plugins: [
@@ -29,7 +30,7 @@ export default defineConfig({
         },
       },
     },
-    ssr: './entry-server.jsx',
+    ssr: path.resolve(__dirname, 'app/entry-server.jsx'),
   },
   esbuild: {
     drop: ['console', 'debugger'],
